54ef4f68d8b3c2525f1ef087af1db4e77452706d
fisker <lionkay@gmail.com>
2026-05-28 15:45:40 +0800

    Refactor

--- index.js
+++ index.js
@@ -1,47 +1,96 @@
-import {find} from './find.js'
+import * as fs from 'node:fs/promises'
+import * as path from 'node:path'
+import process from 'node:process'
+import {toAbsolutePath} from 'url-or-path'
 
 /**
+@import {OptionalUrlOrPath} from 'url-or-path'
 @import {Stats} from 'node:fs'
-@import {NameOrNames, FileOrDirectory, FindOptions, FindResult} from './find.js'
 
-@typedef {(fileOrDirectory: Omit<FileOrDirectory, 'stat'> & {stats: Stats}) => Promise<boolean> | boolean} FileOrDirectoryFilter
+@typedef {(fileOrDirectory: FileOrDirectory) => Promise<boolean> | boolean} Filter
 
-@typedef {Omit<FindOptions, 'names' | 'filter'> & {filter?: FileOrDirectoryFilter}} FileOrDirectoryFindOptions
+@typedef {{
+  name: string,
+  path: string,
+  stats: Stats,
+}} FileOrDirectory
 
-@typedef {FileOrDirectoryFilter | FileOrDirectoryFindOptions} FilterOrOptions
+@typedef {string | string[]} NameOrNames
 
-@typedef {Omit<FileOrDirectoryFindOptions, 'filter'>} OptionsWithoutFilter
+@typedef {{
+  cwd?: OptionalUrlOrPath,
+  allowSymlinks?: boolean,
+  filter?: Filter
+}} FindOptions
+
+@typedef {Filter | FindOptions} FilterOrOptions
+
+@typedef {Omit<FindOptions, 'filter'>} OptionsWithoutFilter
+
+@typedef {Promise<string | undefined>} FindResult
 */
+
+/** @param {FileOrDirectory} file */
+const isFile = (file) => file.stats.isFile()
+/** @param {FileOrDirectory} file */
+const isDirectory = (file) => file.stats.isDirectory()
 
 /**
-@param {NameOrNames} nameOrNames
-@param {(stats: Stats) => boolean} typeCheck
-@param {FilterOrOptions} [filterOrOptions]
-@param {OptionsWithoutFilter} [optionsWithoutFilter]
+Get stats for the given `path`.
+
+@param {string} path
+@param {boolean} allowSymlinks
 */
-function findInDirectoryInternal(
+async function safeStat(path, allowSymlinks) {
+  try {
+    return await (allowSymlinks ? fs.stat : fs.lstat)(path)
+  } catch {
+    // No op
+  }
+}
+
+/**
+Find matched name or names in a directory
+
+@param {NameOrNames} nameOrNames
+@param {FilterOrOptions | undefined} filterOrOptions
+@param {OptionsWithoutFilter | undefined} optionsWithoutFilter
+@param {typeof isFile | typeof isDirectory} [typeCheck]
+*/
+async function findInternal(
   nameOrNames,
-  typeCheck,
   filterOrOptions,
   optionsWithoutFilter,
+  typeCheck,
 ) {
-  const {filter, ...options} =
+  const names = Array.isArray(nameOrNames) ? nameOrNames : [nameOrNames]
+  const options =
     typeof filterOrOptions === 'function'
       ? {...optionsWithoutFilter, filter: filterOrOptions}
       : {...filterOrOptions}
+  const directory = toAbsolutePath(options.cwd) ?? process.cwd()
+  const allowSymlinks = options.allowSymlinks ?? true
+  /** @param {FileOrDirectory} file */
+  const filter = async (file) =>
+    (!typeCheck || typeCheck(file)) &&
+    (!options.filter || (await options.filter(file)))
 
-  return find({
-    ...options,
-    names: nameOrNames,
-    async filter(file) {
-      const stats = await file.stat()
-      return Boolean(
-        stats &&
-        typeCheck(stats) &&
-        (!filter || (await filter({name: file.name, path: file.path, stats}))),
-      )
-    },
-  })
+  for (const name of names) {
+    const fileOrDirectory = path.join(directory, name)
+    const stats = await safeStat(fileOrDirectory, allowSymlinks)
+
+    if (stats) {
+      const file = {
+        name,
+        path: fileOrDirectory,
+        stats,
+      }
+
+      if (await filter(file)) {
+        return fileOrDirectory
+      }
+    }
+  }
 }
 
 /**
@@ -61,11 +110,11 @@ console.log(await findFile(['foo.config.js', 'foo.config.json']))
 ```
 */
 function findFile(nameOrNames, filterOrOptions, optionsWithoutFilter) {
-  return findInDirectoryInternal(
+  return findInternal(
     nameOrNames,
-    (stats) => stats.isFile(),
     filterOrOptions,
     optionsWithoutFilter,
+    isFile,
   )
 }
 
@@ -86,11 +135,11 @@ console.log(await findDirectory(['node_modules', '.yarn']))
 ```
 */
 function findDirectory(nameOrNames, filterOrOptions, optionsWithoutFilter) {
-  return findInDirectoryInternal(
+  return findInternal(
     nameOrNames,
-    (stats) => stats.isDirectory(),
     filterOrOptions,
     optionsWithoutFilter,
+    isDirectory,
   )
 }
 
@@ -109,21 +158,16 @@ import {findInDirectory} from 'find-in-directory'
 console.log(
   await findInDirectory(
     ['node_modules', 'yarn.lock'],
-    ({ stats }) =>
-      (stats.name === 'node_modules' && stats.isDirectory()) ||
-      (stats.name === 'yarn.lock' && stats.isFile()),
+    ({ name, stats }) =>
+      (name === 'node_modules' && stats.isDirectory()) ||
+      (name === 'yarn.lock' && stats.isFile()),
   ),
 );
 // "/path/to/node_modules"
 ```
 */
 function findInDirectory(nameOrNames, filterOrOptions, optionsWithoutFilter) {
-  return findInDirectoryInternal(
-    nameOrNames,
-    () => true,
-    filterOrOptions,
-    optionsWithoutFilter,
-  )
+  return findInternal(nameOrNames, filterOrOptions, optionsWithoutFilter)
 }
 
 export {findDirectory, findFile, findInDirectory}
--- index.test-d.ts
+++ index.test-d.ts
@@ -3,59 +3,67 @@ import {expectType} from 'tsd'
 import {findFile, findDirectory} from './index.js'
 
 // `nameOrNames`
-expectType<string | void>(await findFile('name'))
-expectType<string | void>(await findDirectory('name'))
-expectType<string | void>(await findFile(['a', 'b']))
-expectType<string | void>(await findDirectory(['a', 'b']))
+expectType<string | undefined>(await findFile('name'))
+expectType<string | undefined>(await findDirectory('name'))
+expectType<string | undefined>(await findFile(['a', 'b']))
+expectType<string | undefined>(await findDirectory(['a', 'b']))
 
 // `options`
-expectType<string | void>(await findFile('name', {}))
-expectType<string | void>(await findDirectory('name', {}))
-expectType<string | void>(await findDirectory('name', undefined))
+expectType<string | undefined>(await findFile('name', {}))
+expectType<string | undefined>(await findDirectory('name', {}))
+expectType<string | undefined>(await findDirectory('name', undefined))
 
 // `options.cwd`
-expectType<string | void>(await findFile('name'))
-expectType<string | void>(await findDirectory('name'))
-expectType<string | void>(await findFile('name', {cwd: '/path/to/directory/'}))
-expectType<string | void>(
+expectType<string | undefined>(await findFile('name'))
+expectType<string | undefined>(await findDirectory('name'))
+expectType<string | undefined>(
+  await findFile('name', {cwd: '/path/to/directory/'}),
+)
+expectType<string | undefined>(
   await findDirectory('name', {cwd: '/path/to/directory/'}),
 )
-expectType<string | void>(
+expectType<string | undefined>(
   await findFile('name', {cwd: url.pathToFileURL('/path/to/directory/')}),
 )
-expectType<string | void>(
+expectType<string | undefined>(
   await findDirectory('name', {cwd: url.pathToFileURL('/path/to/directory/')}),
 )
-expectType<string | void>(
+expectType<string | undefined>(
   await findFile('name', {cwd: url.pathToFileURL('/path/to/directory/').href}),
 )
-expectType<string | void>(
+expectType<string | undefined>(
   await findDirectory('name', {
     cwd: url.pathToFileURL('/path/to/directory/').href,
   }),
 )
 
 // `options.filter`
-expectType<string | void>(await findFile('name', {filter: () => true}))
-expectType<string | void>(await findDirectory('name', {filter: () => true}))
-expectType<string | void>(
+expectType<string | undefined>(await findFile('name', {filter: () => true}))
+expectType<string | undefined>(
+  await findDirectory('name', {filter: () => true}),
+)
+expectType<string | undefined>(
   await findFile('name', {filter: () => Promise.resolve(false)}),
 )
-expectType<string | void>(
+expectType<string | undefined>(
   await findDirectory('name', {filter: () => Promise.resolve(false)}),
 )
-expectType<string | void>(
+expectType<string | undefined>(
   await findFile('name', {filter: ({name}) => name === 'name'}),
 )
-expectType<string | void>(
+expectType<string | undefined>(
   await findFile('name', {filter: ({path}) => path === path}),
 )
-expectType<string | void>(
+expectType<string | undefined>(
   await findFile('name', {filter: ({stats}) => stats.mtimeMs < Date.now()}),
 )
 
 // `options.allowSymlinks`
-expectType<string | void>(await findFile('name', {allowSymlinks: true}))
-expectType<string | void>(await findDirectory('name', {allowSymlinks: true}))
-expectType<string | void>(await findFile('name', {allowSymlinks: false}))
-expectType<string | void>(await findDirectory('name', {allowSymlinks: false}))
+expectType<string | undefined>(await findFile('name', {allowSymlinks: true}))
+expectType<string | undefined>(
+  await findDirectory('name', {allowSymlinks: true}),
+)
+expectType<string | undefined>(await findFile('name', {allowSymlinks: false}))
+expectType<string | undefined>(
+  await findDirectory('name', {allowSymlinks: false}),
+)
